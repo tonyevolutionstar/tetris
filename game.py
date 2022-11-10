ebae8f2dd508bb73e791cf3c0520d9100d9e7340
António Ramos <tonyantonio@outlook.pt>
2022-11-10 19:55:25 +0000

    fix image error

--- game.py
+++ game.py
@@ -1,32 +1,32 @@
 import pygame
-import random
 from pygame.locals import*
-from pieces import Piece
+from pieces import *
 from pygame.sprite import *
 from pygame.font import *
 from pygame import *
 
 pygame.init() # starts up pyGame
 
-WIDTH, HEIGHT = 80, 80
+WIDTH, HEIGHT = 40, 60
 SCALE = 10
 
 screen = pygame.display.set_mode((SCALE * WIDTH, SCALE * HEIGHT))
 clock = pygame.time.Clock()
 clock.tick(24)
-all_sprites = pygame.sprite.GroupSingle()
+piece_sprite = pygame.sprite.GroupSingle()
+back_sprite = pygame.sprite.GroupSingle()
 name = Font(None, 16)
 
 pygame.display.set_caption("Tetris")
 pieces = Piece()
-all_sprites.add(pieces)
+background = Background(SCALE, WIDTH, HEIGHT)
+piece_sprite.add(pieces)
+back_sprite.add(background)
+
 
 def main():
     running = 1
     
-    #for i in pieces.images:
-        #print(i)
-    #    display.blit(pieces.images[i], (50,20))
     #pygame.display.flip()
     while running:
         e = event.wait() #pause until event occurs
@@ -34,16 +34,17 @@ def main():
             pygame.quit() #shuts down pyGame
             running = 0
             break
-        screen.fill((255, 255, 255))
 
-        all_sprites.draw(screen)
-        all_sprites.update()
+        screen.fill((255, 255, 255))
+        back_sprite.draw(screen)
+        back_sprite.update()
+        piece_sprite.draw(screen)
+        piece_sprite.update()
+       
         pygame.display.update()
-    #    running = 0
-   
-    #display.update()
-    #pygame.quit() 
+        #pygame.quit()
     
 
 if __name__ == '__main__':  
     main()
+
--- pieces.py
+++ pieces.py
@@ -1,25 +1,32 @@
 import os
 import pygame
+import random 
 from pygame.sprite import *
 
 class Piece(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.path = os.getcwd() + "\\sprite_sheet\\"
-        self.pieces = []
-       
-        #self.images = self.load_pieces()
-        self.image = pygame.image.load(os.path.join(self.path, "1.jpg")).convert()
-        self.image = pygame.transform.scale(self.image, (200, 200)) 
+        pieces_choosed = self.choice_pieces()
+        self.image = pygame.image.load(pieces_choosed).convert()
+        self.image = pygame.transform.scale(self.image, (20, 44)) 
         self.rect = Rect(50, 100, 300, 200)
 
-        #for img in self.images:
-        #    self.image = self.images[img]
+    def next_piece(self):
+        self.path = os.getcwd() + "\\sprite_sheet\\"
+        pieces_choosed = self.choice_pieces()
+        self.image = pygame.image.load(pieces_choosed).convert()
+        self.image = pygame.transform.scale(self.image, (34, 75)) 
+        self.rect = Rect(320, 120, 20, 50)
+        
 
-    def load_pieces(self):
-        filenames = [f for f in os.listdir(self.path) if f.endswith('.jpg')]
-        images = {}
-        for name in filenames:
-            imagename = os.path.splitext(name)[0] 
-            images[imagename] = pygame.image.load(os.path.join(self.path, name)).convert_alpha()
-        return images 
+    def choice_pieces(self):
+        pieces_list = ['1.png', 'L.png', 'R.png', 'S.png', 'T.png']
+        return self.path + random.choice(pieces_list)
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, SCALE, WIDTH, HEIGHT):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.getcwd() + "\\sprite_sheet\\"+'background.jpg').convert()
+        self.image = pygame.transform.scale(self.image, (SCALE * WIDTH, SCALE * HEIGHT)) 
+        self.rect = Rect(0, 0, SCALE * WIDTH, SCALE * HEIGHT)
