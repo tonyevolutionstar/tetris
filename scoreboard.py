c66d5fc31d91b9b4ee9a22a839e1abf2430040a1
António Ramos <tonyantonio@outlook.pt>
2022-11-12 14:00:54 +0000

    add scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,25 @@
+class ScoreBoard:
+    #based on https://tetris.wiki/Scoring
+    def __init__(self):
+        self.score = 0
+        self.level = 1
+        self.action = None
+
+    #state - soft, hard drop
+    def handle_score(self, action, state):
+        if state == "soft":
+            self.score += 1
+        elif state == "hard":
+            self.score += 2
+            
+        if action == "Single":
+            self.score += 100 * self.level
+        elif action == "Double":
+            self.score += 300 * self.level
+        elif action == "Triple":
+            self.score += 500 * self.level 
+        elif action == "Tetris":
+            self.score += 800 * self.level 
+        
+        
+
