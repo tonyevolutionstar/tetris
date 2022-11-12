e94ce38eea86776c43f3344b36447aed9b172024
António Ramos <tonyantonio@outlook.pt>
2022-11-12 20:48:45 +0000

    put labels to a function

--- game.py
+++ game.py
@@ -22,6 +22,23 @@ score_label = Font(None, 20)
 
 pygame.display.set_caption("Tetris")
 
+def labels(score, WIDTH):
+    score_surface = score_label.render("Score", True, (0,0,0))
+    level_surface = score_label.render("Level", True, (0,0,0))
+    lines_surface = score_label.render("Lines", True, (0,0,0))
+    hold_surface = score_label.render("Hold", True, (0,0,0))
+    text_score = score_label.render(str(score.score), True, (132, 202, 255))
+    level_score = score_label.render(str(score.level), True, (132, 202, 255))
+    lines_score = score_label.render(str(score.lines), True, (132, 202, 255))
+    next_piece_label = score_label.render("Next", True,(0,0,0))
+    pygame.Surface.blit(screen, hold_surface, (10, 10))
+    pygame.Surface.blit(screen, score_surface, (WIDTH * SCALE - 100, 10))
+    pygame.Surface.blit(screen, text_score, (WIDTH * SCALE - 100, 25))
+    pygame.Surface.blit(screen, level_surface, (WIDTH * SCALE - 100, 40))
+    pygame.Surface.blit(screen, level_score, (WIDTH * SCALE - 100, 55))
+    pygame.Surface.blit(screen, lines_surface, (WIDTH * SCALE - 100, 70))
+    pygame.Surface.blit(screen, lines_score, (WIDTH * SCALE - 100, 85))
+    pygame.Surface.blit(screen, next_piece_label, (WIDTH * SCALE - 100, 200))
 
 
 def main():
@@ -44,23 +61,7 @@ def main():
 
         screen.fill((255, 255, 255))
         pieces.fill_piece()
-        score_surface = score_label.render("Score", True, (0,0,0))
-        level_surface = score_label.render("Level", True, (0,0,0))
-        lines_surface = score_label.render("Lines", True, (0,0,0))
-        hold_surface = score_label.render("Hold", True, (0,0,0))
-        text_score = score_label.render(str(score.score), True, (132, 202, 255))
-        level_score = score_label.render(str(score.level), True, (132, 202, 255))
-        lines_score = score_label.render(str(score.lines), True, (132, 202, 255))
-        next_piece_label = score_label.render("Next", True,(0,0,0))
-        pygame.Surface.blit(screen, hold_surface, (10, 10))
-
-        pygame.Surface.blit(screen, score_surface, (WIDTH * SCALE - 100, 10))
-        pygame.Surface.blit(screen, text_score, (WIDTH * SCALE - 100, 25))
-        pygame.Surface.blit(screen, level_surface, (WIDTH * SCALE - 100, 40))
-        pygame.Surface.blit(screen, level_score, (WIDTH * SCALE - 100, 55))
-        pygame.Surface.blit(screen, lines_surface, (WIDTH * SCALE - 100, 70))
-        pygame.Surface.blit(screen, lines_score, (WIDTH * SCALE - 100, 85))
-        pygame.Surface.blit(screen, next_piece_label, (WIDTH * SCALE - 100, 200))
+        labels(score, WIDTH)
 
         piece_sprite.update()
        
